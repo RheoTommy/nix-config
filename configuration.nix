bf4f347e55c9b9a0cabe80150e606d0d53f388b9
rheotommy <tommyrheo@gmail.com>
2025-04-22 00:04:49 +0900

    wip

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
     powerManagement.enable = true;
 
@@ -41,9 +41,9 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
@@ -55,7 +55,7 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.production;
   };
-  
+
   networking.hostName = "nixos-desktop"; # Define your hostname.
   networking.networkmanager.enable = true;
   time.timeZone = "Asia/Tokyo";
@@ -77,7 +77,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  
+
 
   services.displayManager.sddm = {
     enable = true;
