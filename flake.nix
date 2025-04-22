b5f0a954e62559909092044b4ec4733a2514c355
rheotommy <tommyrheo@gmail.com>
2025-04-22 12:15:10 +0900

    module化

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
         nixosConfigurations.virtualbox = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
-                ./configuration.nix
+                ./hosts/virtualbox
             ];
         };
     };
